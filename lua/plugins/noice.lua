399c15c6ff1e39be5a65857d11c74fb2fe83839b
r-happy <hr325913@gmail.com>
2025-12-04 00:21:32 +0900

    add

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,6 +2,10 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
 		opts = {
 			lsp = {
 				hover = {
@@ -10,18 +14,19 @@ return {
 				signature = {
 					enabled = true,
 				},
+
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
 			},
 			presets = {
 				bottom_search = true,
 				command_palette = true,
 				long_message_to_split = true,
-
 				lsp_doc_border = true,
 			},
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
 		},
 	},
 }
