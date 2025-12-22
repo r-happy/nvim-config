0513241037825a09df68dc1638f4d61923c90f6a
r-happy <hr325913@gmail.com>
2025-12-22 09:46:59 +0900

    add

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"akinsho/bufferline.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		opts = {
+			options = {
+				separator_style = "thin",
+				mode = "buffers",
+				diagnostics = "nvim_lsp",
+
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					local s = " "
+					for e, n in pairs(diagnostics_dict) do
+						local sym = e == "error" and " " or (e == "warning" and " " or " ")
+						s = s .. n .. sym
+					end
+					return s
+				end,
+			},
+		},
+		keys = {
+			{ "<C-b>p", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<C-b>n", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
+			{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete Other Buffers" },
+		},
+	},
+}
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"folke/trouble.nvim",
+		event = "VeryLazy",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=true win.position=right win.size=40<cr>",
+				desc = "Symbols (Right Sidebar)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+}
