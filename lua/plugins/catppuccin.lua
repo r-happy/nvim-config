1bbedad1d3319ec4f5f9ac0b528a7d27764cb5ae
r-happy <106812882+r-happy@users.noreply.github.com>
2025-12-24 08:51:02 +0900

    change color

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		lazy = false,
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        lazy = false,
+        -- config = function(_, opts)
+        -- 	require("catppuccin").setup(opts)
+        -- 	vim.cmd.colorscheme("catppuccin-mocha")
+        -- end,
+    },
 }
