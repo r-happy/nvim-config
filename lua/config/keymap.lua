ce10a3bc14bc4984874f1db7e0b9d30304b08066
r-happy <106812882+r-happy@users.noreply.github.com>
2025-12-24 09:19:05 +0900

    add

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,50 +1,50 @@
 -- snacks keymaps
 -- begin from "s"
 vim.keymap.set("n", "sf", function()
-	Snacks.picker.smart()
+    Snacks.picker.smart()
 end, { desc = "open picker" })
 
 vim.keymap.set("n", "spg", function()
-	Snacks.picker.grep()
+    Snacks.picker.grep()
 end, { desc = "open grep" })
 
 vim.keymap.set("n", "spl", function()
-	Snacks.picker.lsp_symbols()
+    Snacks.picker.lsp_symbols()
 end, { desc = "open lsp symbols" })
 
 vim.keymap.set("n", "spr", function()
-	Snacks.picker.recent()
+    Snacks.picker.recent()
 end, { desc = "picker recent files" })
 
 vim.keymap.set("n", "se", function()
-	Snacks.explorer()
+    Snacks.explorer()
 end, { desc = "open explorer" })
 
 vim.keymap.set("n", "sg", function()
-	Snacks.lazygit()
+    Snacks.lazygit()
 end, { desc = "open lazygit" })
 
 vim.keymap.set("n", "sb", function()
-	Snacks.picker.buffers()
+    Snacks.picker.buffers()
 end, { desc = "search buffer" })
 
 vim.keymap.set("n", "sl", function()
-	Snacks.picker.lines()
+    Snacks.picker.lines()
 end, { desc = "search buffer lines" })
 
 vim.keymap.set({ "n", "t" }, "st", function()
-	Snacks.terminal()
+    Snacks.terminal()
 end, { desc = "Toggle Terminal" })
 
 -- lsp
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "show hover" })
 vim.keymap.set("n", "gr", function()
-	Snacks.picker.lsp_references()
+    Snacks.picker.lsp_references()
 end, { desc = "goto references" })
 
 -- oil
 vim.keymap.set("n", "<leader>e", function()
-	vim.cmd("Oil")
+    vim.cmd("Oil")
 end, { desc = "open oil" })
 
 -- cursor
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,31 +1,32 @@
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-		opts = {
-			lsp = {
-				hover = {
-					enabled = true, -- これでKがNoiceになる
-				},
-				signature = {
-					enabled = true, -- 引数ヒントもNoiceになる
-				},
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				lsp_doc_border = true,
-			},
-		},
-	},
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        opts = {
+            lsp = {
+                hover = {
+                    enabled = true, -- これでKがNoiceになる
+                },
+                signature = {
+                    enabled = true, -- 引数ヒントもNoiceになる
+                },
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            presets = {
+                bottom_search = true,         -- use a classic bottom cmdline for search
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = true,        -- add a border to hover docs and signature help
+            },
+        },
+    },
 }
